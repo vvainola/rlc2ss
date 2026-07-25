04e833f99f044e45e228176a994bc21da43f1bbc
Vili Väinölä <vilivainola@gmail.com>
2026-07-25 17:06:57 +0300

    feat: select test model at runtime
    Replace compile-time model selection with a runtime model factory and
    export DLL_select_model for configuring model-specific ports and state.

--- src/circuit_tester.cpp
+++ src/circuit_tester.cpp
@@ -22,194 +22,273 @@
 
 #include "integrator.hpp"
 
-#include <complex>
+#include <cstdint>
 #include <memory>
-#include "qucs/diode_matrices.hpp"
-#include "qucs/saturating_inductor_matrices.hpp"
-#include "qucs/mutual_inductor_matrices.hpp"
+#include <vector>
+
+#include "DbgGui/dbg_gui_wrapper.h"
 #include "qucs/controlled_sources_matrices.hpp"
 #include "qucs/converter_matrices.hpp"
-#include "DbgGui/dbg_gui_wrapper.h"
+#include "qucs/diode_matrices.hpp"
+#include "qucs/mutual_inductor_matrices.hpp"
+#include "qucs/saturating_inductor_matrices.hpp"
+
 #ifdef _WIN32
 #define RLC2SS_EXPORT __declspec(dllexport)
 #else
 #define RLC2SS_EXPORT __attribute__((visibility("default")))
 #endif
 
-// #define DIODE_TEST
-// #define RL3
-// #define SATURATING_INDUCTOR
-// #define MUTUAL_INDUCTOR
-// #define CONTROLLED_SOURCES
-#define CONVERTER
+namespace {
 
-#if defined RL3
-Model_RL3 circuit(
-    {.L_a = 1,
-     .L_b = 1,
-     .L_c = 1,
-     .R_a = 10,
-     .R_b = 10,
-     .R_c = 10,
-     .Kab = 0.9934,
-     .Kbc = 0.9934,
-     .Kca = 0.9934});
-#elif defined DIODE_TEST
-Model_diode circuit(Model_diode::Components{
-    .L1 = 1e-2,
-    .L2 = 1e-2,
-    .R1 = 0.1,
-    .R2 = 1.0,
-    .R3 = 1.0,
-    .R_D2 = 1e-3,
-    .R_D3 = 1e-3,
-    .R_D4 = 1e-3,
-});
-#elif defined SATURATING_INDUCTOR
-Model_saturating_inductor circuit(Model_saturating_inductor::Components{});
-#elif defined MUTUAL_INDUCTOR
-Model_mutual_inductor circuit(Model_mutual_inductor::Components{
-    .Cf = 100e-6,
-    .FSRC1 = -100.0,
-    .K12 = 0.5,
-    .K21 = 0.5,
-    .K31 = 0.5,
-    .L1 = 1,
-    .L2 = 1,
-    .L3 = 1,
-    .R1 = 10.0,
-    .R2 = 10.0,
-    .R3 = 10e-3,
-    .R4 = 10.0,
-});
-#elif defined CONTROLLED_SOURCES
-Model_controlled_sources circuit(Model_controlled_sources::Components{
-    .C_1 = 100e-3,
-    .C_2 = 10e-3,
-    .ESRC3 = 1,
-    .FSRC5 = -2.0,
-    .GSRC1 = 30.0,
-    .HSRC4 = 10.0,
-    .L1 = 0.1,
-});
-#elif defined CONVERTER
-Model_converter circuit(Model_converter::Components{
-    .C_a = 10e-3,
-    .C_b = 10e-3,
-    .C_c = 10e-3,
-    .C_n = 100e-3,
-    .C_p = 100e-3,
-    .L_a = 100e-6,
-    .L_b = 100e-6,
-    .L_c = 100e-6,
-    .L_g_a = 100e-6,
-    .L_g_b = 100e-6,
-    .L_g_c = 100e-6,
-    .R_D_a_n = 1e-3,
-    .R_D_a_p = 1e-3,
-    .R_D_b_n = 1e-3,
-    .R_D_b_p = 1e-3,
-    .R_D_c_n = 1e-3,
-    .R_D_c_p = 1e-3,
-    .R_a = 1,
-    .R_b = 1,
-    .R_c = 1,
-    .R_dc = 1,
-    .R_g_a = 1,
-    .R_g_b = 1,
-    .R_g_c = 1,
-    .R_n_p = 1e3,
-    .R_n_s = 10e-3,
-    .R_p_p = 1e3,
-    .R_p_s = 10e-3,
+enum CircuitModel {
+    DIODE = 0,
+    SATURATING_INDUCTOR = 1,
+    MUTUAL_INDUCTOR = 2,
+    CONTROLLED_SOURCES = 3,
+    CONVERTER = 4,
+};
 
-});
-#endif
+class Circuit {
+  public:
+    virtual ~Circuit() = default;
+    virtual int inputCount() const = 0;
+    virtual int outputCount() const = 0;
+    virtual int switchCount() const = 0;
+    virtual double* inputs() = 0;
+    virtual double* outputs() = 0;
+    virtual void init() {}
+    virtual void updateSwitches(uint32_t switches) = 0;
+    virtual void step(double dt) = 0;
+};
 
-double debug[20];
-uint32_t temp;
+template <typename Model>
+class CircuitInstance : public Circuit {
+  public:
+    explicit CircuitInstance(typename Model::Components components)
+        : model(components) {}
 
-extern "C" RLC2SS_EXPORT int DLL_input_count = circuit.NUM_INPUTS;
-extern "C" RLC2SS_EXPORT int DLL_output_count = circuit.NUM_OUTPUTS;
-extern "C" RLC2SS_EXPORT int DLL_switch_count = circuit.NUM_SWITCHES;
-extern "C" RLC2SS_EXPORT double* DLL_inputs = (double*)&circuit.inputs;
-extern "C" RLC2SS_EXPORT uint32_t* DLL_switches = (uint32_t*)&temp; //(uint32_t*)&circuit.switches2;
-extern "C" RLC2SS_EXPORT double* DLL_outputs = (double*)&circuit.outputs;
-extern "C" RLC2SS_EXPORT double* DLL_debug = debug;
+    int inputCount() const override { return Model::NUM_INPUTS; }
+    int outputCount() const override { return Model::NUM_OUTPUTS; }
+    int switchCount() const override { return Model::NUM_SWITCHES; }
+    double* inputs() override { return model.inputs.data.data(); }
+    double* outputs() override { return model.outputs.data.data(); }
+    void init() override {}
+    void updateSwitches(uint32_t) override {}
+    void step(double dt) override { model.step(dt, model.inputs); }
 
-extern "C" RLC2SS_EXPORT void DLL_init(double dt) {
-    // DbgGui_create(dt);
-    DbgGui_startUpdateLoop();
-#if defined SATURATING_INDUCTOR
+    Model model;
+};
+
+template <>
+void CircuitInstance<Model_diode>::updateSwitches(uint32_t switches) {
+    model.switches.S1 = switches & (1U << 0);
+    model.switches.S_D2 = switches & (1U << 1);
+    model.switches.S_D3 = switches & (1U << 2);
+}
+
+template <>
+void CircuitInstance<Model_saturating_inductor>::updateSwitches(
+    uint32_t switches) {
+    model.switches.S1 = switches & (1U << 0);
+    model.switches.S2 = switches & (1U << 1);
+}
+
+template <>
+void CircuitInstance<Model_saturating_inductor>::init() {
     std::vector<double> currents = {0, 1, 2, 5};
     std::vector<double> flux = {0, 0.01, 0.015, 0.0151};
-    double L0 = (flux[1] - flux[0]) / (currents[1] - currents[0]);
-    double L1 = (flux[2] - flux[1]) / (currents[2] - currents[1]);
-    double L2 = (flux[3] - flux[2]) / (currents[3] - currents[2]);
-    double L1_act = (L1 * L0) / (L0 - L1);
-    double L2_act = (L2 * L1_act) / (L1_act - L2);
-    double L1_eff = 1 / (1 / L0 + 1 / L1_act);
-    double L2_eff = 1 / (1 / L0 + 1 / L1_act + 1 / L2_act);
-    circuit.addInductorSaturation(&circuit.components.L0,
-                                  {currents[0], currents[1], currents[2]},
-                                  {L0, L1_eff, L2_eff});
-#elif defined(CONVERTER)
-    std::vector<double> currents = {0, 100,          200,          500};
-    std::vector<double> flux =     {0, 100 * 100e-6, 100 * 120e-6, 100 * 125e-6};
-    double L0 = (flux[1] - flux[0]) / (currents[1] - currents[0]);
-    double L1 = (flux[2] - flux[1]) / (currents[2] - currents[1]);
-    double L2 = (flux[3] - flux[2]) / (currents[3] - currents[2]);
-    double L1_act = (L1 * L0) / (L0 - L1);
-    double L2_act = (L2 * L1_act) / (L1_act - L2);
-    double L1_eff = 1 / (1 / L0 + 1 / L1_act);
-    double L2_eff = 1 / (1 / L0 + 1 / L1_act + 1 / L2_act);
-    circuit.addInductorSaturation(&circuit.components.L_a,
-                                  {currents[0], currents[1], currents[2]},
-                                  {L0, L1_eff, L2_eff});
-    circuit.addInductorSaturation(&circuit.components.L_b,
-                                  {currents[0], currents[1], currents[2]},
-                                  {L0, L1_eff, L2_eff});
-    circuit.addInductorSaturation(&circuit.components.L_c,
-                                  {currents[0], currents[1], currents[2]},
-                                  {L0, L1_eff, L2_eff});
-    /*circuit.addInductorSaturation(&circuit.components.L_g_a,
-                                  {currents[0], currents[1], currents[2]},
-                                  {L0, L1_eff, L2_eff});
-    circuit.addInductorSaturation(&circuit.components.L_g_b,
-                                  {currents[0], currents[1], currents[2]},
-                                  {L0, L1_eff, L2_eff});
-    circuit.addInductorSaturation(&circuit.components.L_g_c,
-                                  {currents[0], currents[1], currents[2]},
-                                  {L0, L1_eff, L2_eff});*/
-#endif
+    const double L0 = (flux[1] - flux[0]) / (currents[1] - currents[0]);
+    const double L1 = (flux[2] - flux[1]) / (currents[2] - currents[1]);
+    const double L2 = (flux[3] - flux[2]) / (currents[3] - currents[2]);
+    const double L1_act = (L1 * L0) / (L0 - L1);
+    const double L2_act = (L2 * L1_act) / (L1_act - L2);
+    const double L1_eff = 1 / (1 / L0 + 1 / L1_act);
+    const double L2_eff = 1 / (1 / L0 + 1 / L1_act + 1 / L2_act);
+    model.addInductorSaturation(&model.components.L0,
+                                {currents[0], currents[1], currents[2]},
+                                {L0, L1_eff, L2_eff});
+}
+
+template <>
+void CircuitInstance<Model_converter>::updateSwitches(uint32_t switches) {
+    constexpr double on_delay = 15e-6;
+    model.switches.S_D_a_n.setOnOffDelays(on_delay, 0);
+    model.switches.S_D_a_p.setOnOffDelays(on_delay, 0);
+    model.switches.S_D_b_n.setOnOffDelays(on_delay, 0);
+    model.switches.S_D_b_p.setOnOffDelays(on_delay, 0);
+    model.switches.S_D_c_n.setOnOffDelays(on_delay, 0);
+    model.switches.S_D_c_p.setOnOffDelays(on_delay, 0);
+
+    model.switches.S_D_a_n = switches & (1U << 0);
+    model.switches.S_D_a_p = switches & (1U << 1);
+    model.switches.S_D_b_n = switches & (1U << 2);
+    model.switches.S_D_b_p = switches & (1U << 3);
+    model.switches.S_D_c_n = switches & (1U << 4);
+    model.switches.S_D_c_p = switches & (1U << 5);
+}
+
+template <>
+void CircuitInstance<Model_converter>::init() {
+    std::vector<double> currents = {0, 100, 200, 500};
+    std::vector<double> flux =
+        {0, 100 * 100e-6, 100 * 120e-6, 100 * 125e-6};
+    const double L0 = (flux[1] - flux[0]) / (currents[1] - currents[0]);
+    const double L1 = (flux[2] - flux[1]) / (currents[2] - currents[1]);
+    const double L2 = (flux[3] - flux[2]) / (currents[3] - currents[2]);
+    const double L1_act = (L1 * L0) / (L0 - L1);
+    const double L2_act = (L2 * L1_act) / (L1_act - L2);
+    const double L1_eff = 1 / (1 / L0 + 1 / L1_act);
+    const double L2_eff = 1 / (1 / L0 + 1 / L1_act + 1 / L2_act);
+    const std::vector<double> saturation_currents =
+        {currents[0], currents[1], currents[2]};
+    const std::vector<double> saturation_inductances =
+        {L0, L1_eff, L2_eff};
+    model.addInductorSaturation(
+        &model.components.L_a, saturation_currents, saturation_inductances);
+    model.addInductorSaturation(
+        &model.components.L_b, saturation_currents, saturation_inductances);
+    model.addInductorSaturation(
+        &model.components.L_c, saturation_currents, saturation_inductances);
+}
+
+std::unique_ptr<Circuit> circuit;
+double debug[20]{};
+uint32_t switch_mask = 0;
+
+std::unique_ptr<Circuit> makeCircuit(int model) {
+    switch (model) {
+    case DIODE:
+        return std::make_unique<CircuitInstance<Model_diode>>(
+            Model_diode::Components{
+                .L1 = 1e-2,
+                .L2 = 1e-2,
+                .R1 = 0.1,
+                .R2 = 1.0,
+                .R3 = 1.0,
+                .R_D2 = 1e-3,
+                .R_D3 = 1e-3,
+            });
+    case SATURATING_INDUCTOR:
+        return std::make_unique<CircuitInstance<Model_saturating_inductor>>(
+            Model_saturating_inductor::Components{});
+    case MUTUAL_INDUCTOR:
+        return std::make_unique<CircuitInstance<Model_mutual_inductor>>(
+            Model_mutual_inductor::Components{
+                .Cf = 100e-6,
+                .FSRC1 = -100.0,
+                .K12 = 0.5,
+                .K21 = 0.5,
+                .K31 = 0.5,
+                .L1 = 1,
+                .L2 = 1,
+                .L3 = 1,
+                .R1 = 10.0,
+                .R2 = 10.0,
+                .R3 = 10e-3,
+                .R4 = 10.0,
+            });
+    case CONTROLLED_SOURCES:
+        return std::make_unique<CircuitInstance<Model_controlled_sources>>(
+            Model_controlled_sources::Components{
+                .C_1 = 100e-3,
+                .C_2 = 10e-3,
+                .ESRC3 = 1,
+                .FSRC5 = -2.0,
+                .GSRC1 = 30.0,
+                .HSRC4 = 10.0,
+                .L1 = 0.1,
+            });
+    case CONVERTER:
+        return std::make_unique<CircuitInstance<Model_converter>>(
+            Model_converter::Components{
+                .C_a = 10e-3,
+                .C_b = 10e-3,
+                .C_c = 10e-3,
+                .C_n = 100e-3,
+                .C_p = 100e-3,
+                .L_a = 100e-6,
+                .L_b = 100e-6,
+                .L_c = 100e-6,
+                .L_g_a = 100e-6,
+                .L_g_b = 100e-6,
+                .L_g_c = 100e-6,
+                .R_D_a_n = 1e-3,
+                .R_D_a_p = 1e-3,
+                .R_D_b_n = 1e-3,
+                .R_D_b_p = 1e-3,
+                .R_D_c_n = 1e-3,
+                .R_D_c_p = 1e-3,
+                .R_a = 1,
+                .R_b = 1,
+                .R_c = 1,
+                .R_dc = 1,
+                .R_g_a = 1,
+                .R_g_b = 1,
+                .R_g_c = 1,
+                .R_n_p = 1e3,
+                .R_n_s = 10e-3,
+                .R_p_p = 1e3,
+                .R_p_s = 10e-3,
+            });
+    default:
+        return nullptr;
+    }
+}
+
+} // namespace
+
+extern "C" RLC2SS_EXPORT int DLL_input_count = 0;
+extern "C" RLC2SS_EXPORT int DLL_output_count = 0;
+extern "C" RLC2SS_EXPORT int DLL_switch_count = 0;
+extern "C" RLC2SS_EXPORT double* DLL_inputs = nullptr;
+extern "C" RLC2SS_EXPORT uint32_t* DLL_switches = &switch_mask;
+extern "C" RLC2SS_EXPORT double* DLL_outputs = nullptr;
+extern "C" RLC2SS_EXPORT double* DLL_debug = debug;
+
+extern "C" RLC2SS_EXPORT int DLL_select_model(int model) {
+    auto selected = makeCircuit(model);
+    if (!selected) {
+        return 0;
+    }
+
+    circuit = std::move(selected);
+    DLL_input_count = circuit->inputCount();
+    DLL_output_count = circuit->outputCount();
+    DLL_switch_count = circuit->switchCount();
+    DLL_inputs = circuit->inputs();
+    DLL_outputs = circuit->outputs();
+    switch_mask = 0;
+    for (double& value : debug) {
+        value = 0;
+    }
+    return 1;
+}
+
+extern "C" RLC2SS_EXPORT void DLL_init(double) {
+    if (!circuit) {
+        return;
+    }
+    circuit->init();
+    // DbgGui_create(dt);
+    DbgGui_startUpdateLoop();
 }
 
 extern "C" RLC2SS_EXPORT void DLL_update(double current_time, double dt) {
-#if defined DIODE_TEST
-    circuit.inputs.V_D2 = 0.1;
-    circuit.inputs.V_D3 = 0.1;
-    circuit.inputs.V_D4 = 0.1;
-    circuit.switches.S1 = temp & 1;
-#elif defined CONVERTER
-    double on_delay = 15e-6;
-    circuit.switches.S_D_a_n.setOnOffDelays(on_delay, 0);
-    circuit.switches.S_D_a_p.setOnOffDelays(on_delay, 0);
-    circuit.switches.S_D_b_n.setOnOffDelays(on_delay, 0);
-    circuit.switches.S_D_b_p.setOnOffDelays(on_delay, 0);
-    circuit.switches.S_D_c_n.setOnOffDelays(on_delay, 0);
-    circuit.switches.S_D_c_p.setOnOffDelays(on_delay, 0);
-
-    circuit.switches.S_D_a_n = temp & 1 << 0;
-    circuit.switches.S_D_a_p = temp & 1 << 1;
-    circuit.switches.S_D_b_n = temp & 1 << 2;
-    circuit.switches.S_D_b_p = temp & 1 << 3;
-    circuit.switches.S_D_c_n = temp & 1 << 4;
-    circuit.switches.S_D_c_p = temp & 1 << 5;
-#endif
-    circuit.step(dt, circuit.inputs);
+    if (!circuit) {
+        return;
+    }
+    circuit->updateSwitches(switch_mask);
+    circuit->step(dt);
     DbgGui_sampleWithTimestamp(current_time);
 }
 
 extern "C" RLC2SS_EXPORT void DLL_terminate() {
     DbgGui_close();
+    circuit.reset();
+    DLL_input_count = 0;
+    DLL_output_count = 0;
+    DLL_switch_count = 0;
+    DLL_inputs = nullptr;
+    DLL_outputs = nullptr;
 }
